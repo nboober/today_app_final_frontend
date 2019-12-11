164ecba3314a4e71fe5f7bb5fe0aa42290723b61
Nick <nboober@terpmail.umd.edu>
2019-12-11 17:38:53 -0500

    added a comment to clothing item component for future reference

--- src/components/ClothingItem.js
+++ src/components/ClothingItem.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom'
 class ClothingItem extends React.Component{
     render(){
         return(
+                // I can't put an anchor tag in an anchor tag. So how do I update clothes while still showing the outfit?????
                 <div>
                     Clothing Item
                 </div>

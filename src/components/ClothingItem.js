545016b5f03cbeffa34b216b67b2d49c575a9d93
Nick <nboober@terpmail.umd.edu>
2019-12-16 15:11:41 -0500

    Fixed an error that would occur if the user does not have any clothes

--- src/components/ClothingItem.js
+++ src/components/ClothingItem.js
@@ -4,14 +4,14 @@ import {Link} from 'react-router-dom'
 class ClothingItem extends React.Component{
     render(){
         return(
-                // I can't put an anchor tag in an anchor tag. So how do I update clothes while still showing the outfit?????
+            this.props.clothingItem ? 
             <Link to="/updateclothingitem" onClick={()=>this.props.selectClothingItem(this.props.clothingItem)}>
                 <div style={{border: "1px solid black", width: "200px", display: "inline-block"}}>
                     <h2>{this.props.clothingItem.name}</h2>
                     <img style={{width: "50px"}} src={this.props.clothingItem.image} alt={this.props.clothingItem.name}/>
                     <p>{this.props.clothingItem.location}</p>
                 </div>            
-            </Link>
+            </Link> : null
         )
     }
 }

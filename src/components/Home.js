15432e38c76c41a5040f4a6c612f31517022397e
Nick <nboober@terpmail.umd.edu>
2019-12-17 10:45:08 -0500

    fixed a bug in my home component where all the outfits clothes weren't being displayed

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,12 +10,12 @@ class Home extends React.Component{
                 <h2>My Outfits</h2>
                 <Filter/>
                 {_.times(this.props.max, (index) => {
-                    let hats = this.props.hats[index] ? this.props.hats[index] : this.props.hats[index-1]
-                    let jewelry = this.props.jewelry[index] ? this.props.jewelry[index] : this.props.jewelry[index-1]
-                    let shirts = this.props.shirts[index] ? this.props.shirts[index] : this.props.shirts[index-1]
-                    let belts = this.props.belts[index] ? this.props.belts[index] : this.props.belts[index-1]
-                    let pants = this.props.pants[index] ? this.props.pants[index] : this.props.pants[index-1]
-                    let shoes = this.props.shoes[index] ? this.props.shoes[index] : this.props.shoes[index-1]
+                    let hats = this.props.hats[index] ? this.props.hats[index] : this.props.hats[0]
+                    let jewelry = this.props.jewelry[index] ? this.props.jewelry[index] : this.props.jewelry[0]
+                    let shirts = this.props.shirts[index] ? this.props.shirts[index] : this.props.shirts[0]
+                    let belts = this.props.belts[index] ? this.props.belts[index] : this.props.belts[0]
+                    let pants = this.props.pants[index] ? this.props.pants[index] : this.props.pants[0]
+                    let shoes = this.props.shoes[index] ? this.props.shoes[index] : this.props.shoes[0]
 
                     return <OutfitContainer 
                                 key={index}

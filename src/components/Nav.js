67dc7e6fe43107da1d8817b9e6f0530f2c0ff53a
Nick <nboober@terpmail.umd.edu>
2019-12-28 13:37:48 -0500

    added a title to the navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,31 +12,34 @@ class Nav extends React.Component{
     render() {
         return(
            
-// navbar
-<div class="pos-f-t">
-  <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-4">
-      {/* <h4 class="text-white">Collapsed content</h4>
-      <span class="text-muted">Toggleable via the navbar brand.</span> */}
-        {this.props.user ? (
-            <NavLink to="/"><p>Clothes for Today</p></NavLink>
-            ):(null)}
+        // navbar
+        <div class="pos-f-t">
+          <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4">
+              {/* <h4 class="text-white">Collapsed content</h4>
+              <span class="text-muted">Toggleable via the navbar brand.</span> */}
+                {this.props.user ? (
+                    <NavLink to="/"><p>Clothes for Today</p></NavLink>
+                    ):(null)}
 
-        {this.props.user ? (
-            <NavLink to="/profile" name="Profile"><p>Profile</p></NavLink>
-            ):(null)}
-        
-        {this.props.user ? (
-            <NavLink to="/login" name="Logout" onClick={this.logout} ><p>Logout</p></NavLink>
-            ):(null)}
-    </div>
-  </div>
-  <nav class="navbar navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </nav>
-</div>
+                {this.props.user ? (
+                    <NavLink to="/profile" name="Profile"><p>Profile</p></NavLink>
+                    ):(null)}
+                
+                {this.props.user ? (
+                    <NavLink to="/login" name="Logout" onClick={this.logout} ><p>Logout</p></NavLink>
+                    ):(null)}
+            </div>
+          </div>
+          <nav class="navbar navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            
+          {/* <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt=""/> */}
+          <span style={{color: "white", fontSize: "32px"}}>TODAY</span>
+          </nav>
+        </div>
 
 
         )
